043a5adde7872acb29c54781452a5f073ce7f840
Jason Ho <jason.ho19@outlook.com>
2025-05-25 23:24:01 +1000

    Update rtl_433_devices.h to only use WS90

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -279,13 +279,11 @@
  * Subset of devices that I have access to and have tested with
  */
 #  define DEVICES         \
-    DECL(acurite_986)     \
-    DECL(skylink_motion)  \
-    DECL(prologue)        \
-    DECL(philips_aj3650)  \
-    DECL(fineoffset_WH51) \
+    DECL(fineoffset_ws90)            \
 /* Add new personal decoders here. */
-#  define NUMOFDEVICES 5
+#  define NUMOFDEVICES 1
+#  define NUMOF_OOK_DEVICES 1 
+#  define NUMOF_FSK_DEVICES 1
 #endif
 
 #define DECL(name) extern r_device name;
